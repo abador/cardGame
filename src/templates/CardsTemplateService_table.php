c83a074f575e257503c176231d3912a72f66d80f
Martyna Nowicka <martynaanow@gmail.com>
2020-11-18 16:58:31 +0100

    Display 3 items per row

--- src/templates/CardsTemplateService_table.php
+++ src/templates/CardsTemplateService_table.php
@@ -1,10 +1,10 @@
 <div class="cards-game-main-div">
 <h1>Welcome to your card collection!</h1><br>
-<p>This is description of this special page with special collection of a cards! It contain all cards that you can get when visiting our wikis. Visit more wikis to unlock more fluffy animals!</p><br>
-	<table border="0" cellpadding="2">
+<p>This is description of this special page with special collection of a cards. Bla bla bla.</p><br>
+	<table width="250" border="0" cellpadding="2">
 	<?php $counter = 1; ?>
 		<?php foreach ( $images as $image ): ?>
-				<? if ( $counter === 1 ) : ?>
+				<? if ( $counter % 4 === 0 ) : ?>
 					<tr>
 				<? endif; ?>
 				<td class="cards-game-td" align="center" valign="center">
@@ -16,10 +16,10 @@
 					<p id="card-game-card-text-main"><?= Sanitizer::encodeAttribute( $image['card_name'] ) ?></p>
 					<p id="card-game-card-text-description"><?= Sanitizer::encodeAttribute( $image['card_description'] ) ?></p>
 				</td>
-				<?php if( $counter === 4): $counter = -1;?>
+				<? if ( $counter % 3 === 0 ) : ?>
 					</tr>
-				<?php endif; ?>
-				<?php $counter++; ?>
+				<? endif; ?>
+				<?php $counter += 1; ?>
 			<?php endforeach ?>
 	</table>
 </div>
