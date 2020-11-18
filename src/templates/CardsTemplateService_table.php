1a601c7338dfa86d9994124bbfda1e31fdce4434
Martyna Nowicka <martynaanow@gmail.com>
2020-11-18 15:11:53 +0100

    Make special page prettier

--- src/templates/CardsTemplateService_table.php
+++ src/templates/CardsTemplateService_table.php
@@ -1,19 +1,25 @@
-<div>
-	<?php foreach ( $images as $image ): ?>
-		<tr>
-			<td>
-				<? if ( $image['achived'] ) : ?>
-					<img src="<?= $image['card_url']; ?>" width="200"/>
-				<? else: ?>
-					<img src="<?= $image['card_url']; ?>" width="200" style="filter: blur(5px)"/>
+<div class="cards-game-main-div">
+<h1>Welcome to your card collection!</h1><br>
+<p>This is description of this special page with special collection of a cards. Bla bla bla.</p><br>
+	<table width="250" border="0" cellpadding="2">
+	<?php $counter = 1; ?>
+		<?php foreach ( $images as $image ): ?>
+				<? if ( $counter % 4 === 0 ) : ?>
+					<tr>
 				<? endif; ?>
-			</td>
-			<td>
-				<p><?= Sanitizer::encodeAttribute( $image['card_name'] ) ?></p>
-			</td>
-			<td>
-				<p><?= Sanitizer::encodeAttribute( $image['card_description'] ) ?></p>
-			</td>
-		</tr>
-	<?php endforeach ?>
+				<td class="cards-game-td" align="center" valign="center">
+					<? if ( $image['achived'] ) : ?>
+						<img src="<?= $image['card_url']; ?>" width="200"/>
+					<? else: ?>
+						<img class="card-game-blurred-image" src="<?= $image['card_url']; ?>" width="200"/>
+					<? endif; ?>
+					<p id="card-game-card-text-main"><?= Sanitizer::encodeAttribute( $image['card_name'] ) ?></p>
+					<p id="card-game-card-text-description"><?= Sanitizer::encodeAttribute( $image['card_description'] ) ?></p>
+				</td>
+				<? if ( $counter % 3 === 0 ) : ?>
+					</tr>
+				<? endif; ?>
+				<?php $counter += 1; ?>
+			<?php endforeach ?>
+	</table>
 </div>
